a2e53a3d6a155a833510018ab728139804240b91
wimble3 <wimble@internet.ru>
2025-07-16 22:04:06 +0300

    shortcuts for select next, prev, accept autocomplits

--- init.lua
+++ init.lua
@@ -848,8 +848,9 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<S-Enter>'] = { 'accept', 'fallback' },
       },
 
       appearance = {

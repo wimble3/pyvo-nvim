d93fe4c89e8c43e667cc886f243ba6eecf60bf8d
wimble3 <wimble@internet.ru>
2025-07-17 13:22:52 +0300

    autosaving plugin

--- init.lua
+++ init.lua
@@ -895,6 +895,17 @@ require('lazy').setup({
   {'doums/darcula'},
 
   {
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
+
+  {
     "pustota-theme/pustota.nvim",
     version = "*",
     dependencies = { "nvim-treesitter/nvim-treesitter" },

749395008915f6d9c34c775becdf7b14edfccca5
wimble3 <wimble@internet.ru>
2025-07-18 11:02:52 +0300

    added petterns for autosaving

--- init.lua
+++ init.lua
@@ -900,8 +900,13 @@ require('lazy').setup({
     cmd = "ASToggle", -- optional for lazy loading on command
     event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {
-      -- your config goes here
-      -- or just leave it empty :)
+      {
+        trigger_events = {
+          immediate_save = {
+            { "BufLeave", pattern = { "*.py", "*.js", "*.html", "*.css", ".scss"} }
+          }
+        }
+      }
     },
   },
 
